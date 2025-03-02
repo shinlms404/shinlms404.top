7f4612995aa95e3a405c4993656e8359e98d7e3a
Charlie Chan <shinlms404@gmail.com>
2025-03-02 21:54:03 +0800

    chore: add baidu-site-verification meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2025-03-02",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "nuxt-lodash", "@nuxt/icon"],
-  compatibilityDate: "2025-03-02"
+  app: {
+    head: {
+      meta: [{ name: "baidu-site-verification", content: "codeva-oBnKCLc0P8" }]
+    }
+  }
 });
