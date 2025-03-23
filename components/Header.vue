61b1c39682c14330334f6a6b8c8ca331d9c4ada4
Charlie Chan <shinlms404@gmail.com>
2025-03-23 20:30:08 +0800

    feat: add back btn

--- components/Header.vue
+++ components/Header.vue
@@ -4,11 +4,19 @@ defineProps({
 })
 
 const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+const route = useRoute()
+const router = useRouter()
 </script>
 
 <template>
-  <header class="flex justify-between px-10 min-h-15">
-    <nav />
+  <header class="flex justify-between items-center px-4 min-h-15">
+    <div>
+      <button v-if="route.path !== '/'" class="flex justify-center items-center w-fit h-full hover:text-gray-500" @click="router.go(-1)">
+        <Icon name="material-symbols-light:arrow-back-2-rounded" size="1.5rem" />
+        <span text="1rem">back</span>
+      </button>
+    </div>
     <div class="flex items-center gap-5">
       <Time />
       <Me />
