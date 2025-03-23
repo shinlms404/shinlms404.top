5a4d6f618d96cce575bc1a705a048b46323b9b24
Charlie Chan <shinlms404@gmail.com>
2025-03-23 20:56:19 +0800

    refactor: comment out ProjectLinks component in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="flex flex-col justify-center items-start p-8 max-w-7xl">
     <ProfileSection />
-    <ProjectLinks />
+    <!-- <ProjectLinks /> -->
     <SocialLinks />
     <SupportLinks />
   </div>
